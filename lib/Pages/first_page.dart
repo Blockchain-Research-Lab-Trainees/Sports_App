3972236cc1d04c2de4b15f0751f0b590d59b9997
Ananya Singhal <singhalananya67@gmail.com>
2023-10-29 14:36:36 +0530

    Profile

---
+++ lib/Pages/first_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:sports_app/Elements/bnb.dart';
+// ignore_for_file: prefer_const_constructors
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/sport.png'),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Sports App',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => BottomNavBar()),
+                );
+              },
+              child: Text('Enter'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
